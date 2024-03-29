e2cf645556c23825614857b9d61eda48b12a3e4e
Simone <simone.compri@gmail.com>
2024-03-29 15:55:09 +0100

    Created global css

--- src/main.ts
+++ src/main.ts
@@ -60,6 +60,9 @@ import "@ionic/vue/css/display.css";
 /* Theme variables */
 import "./theme/variables.css";
 
+/* Global styles */
+import "./theme/global.css";
+
 const app = createApp(App)
   .use(store)
   .use(IonicVue)
